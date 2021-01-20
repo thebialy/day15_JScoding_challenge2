bc9ee49beeacdd36dcb1d428ec27914729b6ec40
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-20 08:43:56 -0600

    largest number in arrays completed

--- script.js
+++ script.js
@@ -6,7 +6,19 @@
 // Return Largest Numbers in Arrays
 
 const largestOfFour = (arr) => {
-
+    const max = []
+    
+    for (let i=0; i < arr.length; i++) {
+        let currentMax = arr[i][0]
+        for(let j=0; j < arr[i].length; j++) {
+            let currentNum = arr[i][j]
+            if(currentNum >= currentMax) {
+                currentMax = currentNum
+            }
+        }
+        max.push(currentMax)
+    }
+    console.log(max)
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18,26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
