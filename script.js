3ae80ed3818e8663805dd95d8d6bdfa38433fe61
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-03 10:22:59 -0500

    commit

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const largestOfFour = (arr) => {
 largestOfFour([[4, 5, 1, 3], [13, 27, 18,26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
 
 
-// Challenge 2
+// Challenge 2 //
 // Confirm the Ending
 
 const confirmEnding = (str, target) => {
